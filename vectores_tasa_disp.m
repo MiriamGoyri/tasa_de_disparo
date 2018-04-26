73d8d7977df1a291cb971547b9ff9fd53b9dae9d
Unknown <miriam@lafuentelab.org>
2018-04-26 10:15:09 -0500

    descripción del análisis

---
+++ vectores_tasa_disp.m
@@ -0,0 +1,11 @@
+% !. Conseguir los vectores!!!!
+
+% 2. Seleccionar el metodo  para cortar los vectores (usando el mas largo,
+% el mas corto o cortar algo???
+
+
+% 3. Matriz e vectores
+
+% 4. PCA
+
+% 5. clusterizacion
